0d61e84a818ab9a42f290622f9d96e7cb99647ef
Kanishk <kanishkkumar127@gmail.com>
2024-12-27 22:49:28 +0530

    Forming the Basic Template

---
+++ app/(root)/ApplyForLoan/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ApplyForLoans = () => {
+  return (
+    <div>ApplyForLoans</div>
+  )
+}
+
+export default ApplyForLoans
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-6xl text-blue-500">
+        Lend Sphere
+      </h1>
+    </div>
+  );
+}
