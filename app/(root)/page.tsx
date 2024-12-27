dab439dd693dc1b77092174f258898b0fb21d192
Kanishk <kanishkkumar127@gmail.com>
2024-12-28 02:01:45 +0530

    Navbar Done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl text-blue-500">
+      <h1 className="text-6xl text-blue-500 flex justify-center items-center min-h-screen">
         Lend Sphere
       </h1>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,44 @@
-// components/Navbar.tsx
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white border-b border-blue-500">
-      <div className="container mx-auto px-4 flex justify-between items-center h-16">
-        {/* Logo */}
-        <div className="text-blue-600 text-xl font-bold">
-          <a href="/">LOGO</a>
+    <nav className="bg-white fixed top-0 left-0 w-full flex items-center justify-between px-20">
+      <div className="flex items-center space-x-10">
+        <div className="mt-2">
+          <Link href="/">
+            <Image
+              src="/LogoFull.png"
+              alt="Logo Full"
+              height={60}
+              width={150}
+            />
+          </Link>
         </div>
-        
-        {/* Navigation Links */}
-        <div className="space-x-8 text-blue-600 font-medium hidden sm:flex">
-          <a href="/bonds" className="hover:underline">
+
+        <div className="hidden sm:flex items-center space-x-10 text-blue-950 font-bold text-md font-serif">
+          <Link href="/bonds" className="hover:text-gray-500">
             All Bonds
-          </a>
-          <a href="/investments" className="hover:underline">
+          </Link>
+          <Link href="/investments" className="hover:text-gray-500">
             My Investments
-          </a>
-          <a href="/loans" className="hover:underline">
+          </Link>
+          <Link href="/loans" className="hover:text-gray-500">
             Apply For Loans
-          </a>
+          </Link>
         </div>
+      </div>
+
+      <div className="hidden sm:flex items-center text-blue-950 font-bold text-md font-serif">
+        <Link href="/admin" className="hover:text-gray-500">
+          Admin Login
+        </Link>
+        <Link
+          href="/admin"
+          className="hover:text-gray-500 border-2 border-black rounded-lg m-4 px-4 py-2"
+        >
+          Connect Wallet
+        </Link>
       </div>
     </nav>
   );
