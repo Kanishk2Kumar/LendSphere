669be4fdd980796f3ef1bb9d54159f6a7c4313ae
Kanishk <kanishkkumar127@gmail.com>
2024-12-28 09:43:37 +0530

    Merged Nav, FAQs, Footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,9 @@ import Footer from '../../components/footer';
 function App() {
   return (
     <div>
+      <div className='flex justify-center items-center min-h-screen text-6xl'>
+        <h1>Hero Section</h1>
+      </div>
       <FAQSection />
       <Footer />
     </div>
